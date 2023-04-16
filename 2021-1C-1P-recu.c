60220d3af57994461fb0bf51a470a6d0ad63b52f
pedgarcia1 <pedgarcia@itba.edu.ar>
2023-04-16 19:35:10 -0300

    todos los ejs

--- 2021-1C-1P-recu.c
+++ 2021-1C-1P-recu.c
@@ -1,12 +1,34 @@
 #include<stdio.h>
+#include<string.h>
 
 void tri(int n);
+int str_lgth(char *str1);
+int is_in_str(char *str1,char *str2);
+
+#define X 20
 
 void main()
 {
     int n = 7;
 
     tri(n);
+
+    char s1[] = "hola";
+    char s2[] = "ona";
+
+    int flag = is_in_str(s2,s1);
+
+    // printf("%d \n",flag);
+
+    char d = 50;
+    printf("%X\n",d);   
+    char x = 153;
+    printf("%d \n",x);
+
+    /*
+    ej4. a) gcc main.c -Wall -o recuperatorio.exe -g -D DEBUG
+    b) .\recuperatorio.exe
+    */
 
 }
 
@@ -39,4 +61,44 @@ void tri(int n)
     
 
     
+}
+
+int is_in_str(char *str1,char *str2)
+{
+    int largo1 = str_lgth(str1);
+    int largo2 = str_lgth(str2);
+
+    int flag = 0;
+
+    for (int i = 0; i < largo2; i++)
+    {
+        if (str1[0] == str2[i])
+        {
+            flag = 1;
+            for (int j = 0; j < largo1; j++)
+            {
+                if (str1[j] != str2[i+j])
+                {
+                    flag = 0;
+                }
+                
+            }
+            
+        }
+        
+        
+    }
+    return flag;
+    
+    
+}
+
+int str_lgth(char *str1)
+{
+    int i = 0;
+    while (str1[i] != '\0')
+    {
+        i++;
+    }
+    return i;
 }
--- 2021-1C-1P.c
+++ 2021-1C-1P.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
 #include <math.h>
 
+#define N_TERM 12
+
 double prodEscalar(double *v1,double *v2, int largo);
+double seno_iter(double x);
+int factorial(int x);
 
 // faltan comentarios
 void main()
@@ -32,6 +36,8 @@ void main()
     printf("%010f \n", (double) (var--/3));
     // char otro[];
 
+    double x = 0.3; //angulo en rad
+    printf("%f %f",seno_iter(x),sin(x));
     
 }
 
@@ -44,5 +50,30 @@ double prodEscalar(double *v1,double *v2, int largo)
     }
     return out;
 
+}
+
+double seno_iter(double x)
+{
+    // int f,termi;
+    double res = 0;
+    int count = 0;
+    for (int i = 1; i < N_TERM; i = i+2)
+    {
+        // f = factorial(i);
+        // termi = pow(-1,count);
+        res += pow(-1,count)*pow(x,i)/(factorial(i));
+        count++;
+    }
+    return res;
+}
+
+int factorial(int x)
+{
+    int res = 1;
+    for (int i = 0; i < x; i++)
+    {
+        res = res*(x-i);
+    }
+    return res;
 }
 
