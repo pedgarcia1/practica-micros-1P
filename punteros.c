dfcf13d43dceb411c8f9195b37c51fa786c76745
Teo Nicoletti <130874940+TeoNicoletti@users.noreply.github.com>
2023-04-19 11:59:06 -0300

    Repaso de punteros

---
+++ punteros.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    void *puntero = NULL;
+    int *pint;
+
+    puntero = calloc(10, sizeof(int));
+
+    pint = puntero;
+
+    pint[0] = 10;
+    pint[1] = 20;
+    pint[2] = 30;
+
+    char array[12];
+   
+    for (int i = 0; i < 12; i++)
+    {
+        array[i] = ((char *) puntero)[i];
+
+    }
+    // print the array
+    for (int i = 0; i < 12; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+    // wait for Q to be pressed
+    //while (getchar() != 'q');
+    free(puntero);
+
+    return 0;
+}
