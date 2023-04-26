866a38e2ea66f866b9ae56cdb3e932f717cb4727
pedgarcia1 <pedgarcia@itba.edu.ar>
2023-04-26 19:25:08 -0300

    blink v2

--- microC/blink2.c
+++ microC/blink2.c
@@ -15,7 +15,7 @@ void main(void)
     #define PIN_MSK    (1<<5) // P1.5
     #define RED_GREEN_MSK (1<<0)
 
-    srand(time(NULL)); // inicio semilla rand
+    // srand(time(NULL)); // inicio semilla rand
 
     WDTCTL = WDTPW | WDTHOLD; // stop watchdog timer
     P1DIR |= LED_GREEN_MSK ^ RED_GREEN_MSK ^ PIN_MSK; // configure Pin as output
@@ -23,19 +23,23 @@ void main(void)
 
 
     volatile unsigned int i;
+    volatile unsigned int iter;
+    volatile unsigned int frecuencia = 100;
+    iter = 20000*2.59/frecuencia;
+
 
     while(1)
     {
-        if(rand()%2)
+        if(1)
         {
 	        P1OUT ^= LED_GREEN_MSK; // Toggle PIN
         }
-        if(rand()%2)
+        if(1)
         {
             P1OUT ^= RED_GREEN_MSK;
             // P1OUT ^= PIN_MSK;
         }
-	        for(i=20000; i>0; i--) {} //  Some delay
+	        for(i=iter; i>0; i--) {} //  Some delay
     }
 
 
